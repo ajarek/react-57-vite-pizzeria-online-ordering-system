2ddd77dee8b5a134679c945b94fd7182554ab87f
Ajarek <ajarek@poczta.onet.pl>
2023-02-01 00:51:54 +0100

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+     <h1> Pizzeria Paradise</h1>
+       <a href="https://vitejs.dev" target="_blank">
+    <img src="/pizza/icon.png" className="logo" alt="Vite logo" />
+  </a>
+  </div>
+  )
+}
+
+export default Header
