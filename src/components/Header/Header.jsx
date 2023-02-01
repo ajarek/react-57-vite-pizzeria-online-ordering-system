141beb23945b40c04d0ce888bee53876bc26e102
Ajarek <ajarek@poczta.onet.pl>
2023-02-02 00:57:01 +0100

    added section pizza

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,10 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
+      <div className="wrapper-header">
+      <h1 className='header-h1'>Pizzeria Paradise</h1>
+      <h3 className='header-h3'>Submit Your Order</h3>
+      </div>
      <Logo/>
      <Navigation/>
     </div>
---
+++ src/components/PizzaSection/PizzaSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './PizzaSection.css'
+const PizzaSection = () => {
+  return (
+    <section className='pizza-section'>
+      <div className="menu-btn">
+        <button>Menu Restrictions</button>
+      </div>
+    </section>
+  )
+}
+
+export default PizzaSection
