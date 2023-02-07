80410e19818468f8eb2f1a3e1098ef217cabeefc
Ajarek <ajarek@poczta.onet.pl>
2023-02-07 23:58:24 +0100

    added close to Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,17 @@
 import React ,{useEffect, useState, useContext} from 'react'
+import { IoMdCloseCircle } from 'react-icons/io';
 import { AppContext } from '../../App'
 import './Cart.css'
 const Cart = () => {
-  const {items, setItems, count, setCount, valueAll,addToCart,setAddToCart} = useContext(AppContext)
+  const {items, setItems, count, setCount, valueAll,addToCart,setAddToCart,openCart,setOpenCart} = useContext(AppContext)
   return (
-    <div>
-      lol
+    <div className='cart'>
+     <div
+       className="close"
+       onClick={()=>setOpenCart(null)}
+       >
+        <IoMdCloseCircle/>
+        </div>
     </div>
   )
 }
